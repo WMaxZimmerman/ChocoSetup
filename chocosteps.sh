220cec1c2cd84253704e7dc0925cbc80b6c08d2f
Max Zimmerman <william.m.zimmerman@gmail.com>
2019-03-30 08:13:04 -0500

    Added script for choco installs

---
+++ chocosteps.sh
@@ -0,0 +1,21 @@
+choco install git -y
+choco install cmder -y
+
+choco install netfx-4.7.1-devpack -y
+choco install dotnetcore-sdk -y
+choco install python
+choco install jre8
+
+choco install clcl.portable -y
+choco install 7zip -y
+choco install launchy -y
+choco install emacs -y
+choco install visualstudio2017community -y
+
+choco install slack -y
+choco install dropbox -y
+choco install googlechrome -y
+choco install vlc -y
+choco install sharpkeys -y
+
+choco install steam -y
