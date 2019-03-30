827303812a918a0ea1dd7da9d5902b11238ab4bc
Max Zimmerman <william.m.zimmerman@gmail.com>
2019-03-30 08:45:17 -0500

    Added in more pacakages and chrome extensions doc

--- chocosteps.sh
+++ chocosteps.sh
@@ -1,21 +1,25 @@
-choco install git -y
-choco install cmder -y
-
 choco install netfx-4.7.1-devpack -y
 choco install dotnetcore-sdk -y
 choco install python
 choco install jre8
+choco install nodejs.install -y
 
 choco install clcl.portable -y
 choco install 7zip -y
 choco install launchy -y
+choco install f.lux -y
+
 choco install emacs -y
+choco install vscode -y
 choco install visualstudio2017community -y
 
-choco install slack -y
 choco install dropbox -y
 choco install googlechrome -y
 choco install vlc -y
 choco install sharpkeys -y
 
+choco install slack -y
 choco install steam -y
+choco install mumble -y
+choco install discord -y
+choco install logitechgaming -y
