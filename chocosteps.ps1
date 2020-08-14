ac1f1fc0558bcf4fefc5fe10c41952521c1a91d4
Max Zimmerman <william.m.zimmerman+github@gmail.com>
2020-08-14 11:12:43 -0500

    Updated choco packages

--- chocosteps.ps1
+++ chocosteps.ps1
@@ -1,30 +1,36 @@
+# Language Runtimes
 choco install netfx-4.7.1-devpack -y
 choco install dotnetcore-sdk -y
 choco install python -y
 choco install jre8 -y
-choco install nodejs.install -y
+choco install nodejs.install -y 
+choco install lein -y
 
+# Usability
 choco install clcl.portable -y
 choco install 7zip -y
 choco install launchy -y
 choco install f.lux -y
+choco install sharpkeys -y
 
+# Editors
 choco install emacs -y
 choco install vscode -y
-choco install visualstudio2017community -y
+choco install visualstudio2019community -y
 
+# Applications
 choco install dropbox -y
 choco install googlechrome -y
 choco install vlc -y
-choco install sharpkeys -y
-
 choco install slack -y
+choco install plantuml -y
+choco install pandoc -y
+choco install libreoffice-fresh -y
+
+# Gaming
 choco install steam -y
-choco install mumble -y
 choco install discord -y
 choco install logitechgaming -y
 
+# Database
 choco install postgres -y
-choco install lein -y
-
-
