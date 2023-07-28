61eabfca8e366528c3e905193c0a053036cd5013
Max Zimmerman <WMaxZimmerman@users.noreply.github.com>
2023-07-28 16:19:43 -0500

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,9 +1,12 @@
 # === Setup Directories ===
-mkdir 'c:/git'
-mkdir 'c:/tools'
+mkdir 'c:/bench'
+mkdir 'c:/bench/git'
+mkdir 'c:/bench/tools'
+mkdir 'c:/temp'
 
 # === Install Chocolatey ===
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+irm get.scoop.sh -outfile '~/install.ps1'
 
 # === Download Repo ===
 invoke-webrequest -uri https://github.com/WMaxZimmerman/ChocoSetup/archive/master.zip -outfile c:/temp/setup.zip
