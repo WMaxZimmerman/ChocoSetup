530d7a0f2c0e36e76d7cd4c001b16da2cdef37fa
Max Zimmerman <WMaxZimmerman@users.noreply.github.com>
2023-07-28 16:36:34 -0500

    updated with failed packages

--- chocosteps.ps1
+++ chocosteps.ps1
@@ -1,36 +1,36 @@
 # Language Runtimes
-scoop install dotnet
+#scoop install dotnet
 scoop install python
-scoop install jre8
+#scoop install jre8
 scoop install nodejs 
 
 # Usability
 scoop install git
-scoop install clcl.portable
+#scoop install clcl.portable
 scoop install 7zip
-scoop install launchy
-scoop install f.lux
-scoop install sharpkeys
+#scoop install launchy
+#scoop install f.lux
+#scoop install sharpkeys
 scoop install cmder-full
 
 # Editors
-scoop install emacs
-scoop install vscode
+#scoop install emacs
+#scoop install vscode
 # scoop install visualstudio2019community
 
 # Applications
-scoop install dropbox
-scoop install googlechrome
+#scoop install dropbox
+#scoop install googlechrome
 # scoop install vlc
-scoop install slack
-scoop install plantuml
+#scoop install slack
+#scoop install plantuml
 scoop install pandoc
 scoop install jq
 # scoop install libreoffice-fresh
 
 # Gaming
 # scoop install steam
-scoop install discord
+#scoop install discord
 # scoop install logitechgaming
 
 # Database
