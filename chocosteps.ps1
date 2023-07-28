1e1fc4e992f726746ae56d6654dab2e13cb56e1c
Max Zimmerman <WMaxZimmerman@users.noreply.github.com>
2023-07-28 16:45:38 -0500

    Update chocosteps.ps1

--- chocosteps.ps1
+++ chocosteps.ps1
@@ -1,36 +1,40 @@
+scoop bucket add extras
+scoop bucket add java
+
 # Language Runtimes
-#scoop install dotnet
+scoop install dotnet-sdk
 scoop install python
-#scoop install jre8
+scoop install oraclejre8
 scoop install nodejs 
 
 # Usability
 scoop install git
 #scoop install clcl.portable
 scoop install 7zip
-#scoop install launchy
-#scoop install f.lux
-#scoop install sharpkeys
+scoop install launchy
+scoop install flux
+scoop install sharpkeys
 scoop install cmder-full
 
 # Editors
-#scoop install emacs
-#scoop install vscode
+scoop install emacs
+scoop install vscode
+scoop install rider
 # scoop install visualstudio2019community
 
 # Applications
-#scoop install dropbox
-#scoop install googlechrome
-# scoop install vlc
-#scoop install slack
-#scoop install plantuml
+# scoop install dropbox
+scoop install googlechrome
+scoop install vlc
+scoop install slack
+scoop install plantuml
 scoop install pandoc
 scoop install jq
 # scoop install libreoffice-fresh
 
 # Gaming
 # scoop install steam
-#scoop install discord
+scoop install discord
 # scoop install logitechgaming
 
 # Database
